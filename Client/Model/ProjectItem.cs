821e5f6ed5fbd38c8352ef1a1ee473451bc6c4a3
davin <copperbeard@protonmail.com>
2022-07-06 15:17:40 +0100

    update portfolio page
    change the porfolio page to auto grab repos from github

---
+++ Client/Model/ProjectItem.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System;
+
+namespace Client.Model;
+
+public class ProjectItem
+{
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string URI { get; set; }
+}
